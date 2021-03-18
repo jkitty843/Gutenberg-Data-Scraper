d531e519ccc25f4b51ea6a6206cdce49144ac928
jkitty843 <jcaitlynaltman@gmail.com>
2021-03-18 09:10:15 -0400

    Moved UI dependent functions into UI file

--- GutenbergLookup.py
+++ GutenbergLookup.py
@@ -39,18 +39,3 @@ def compare_regex_instances(books, search_regex_string, book_dict):
         result_string += (f'{book_dict[book[0]]} contains {instances} instances of the searched regex {search_regex_string}'+'\n')
         result_dict.update({book_dict[book[0]] : instances})
     return result_string, result_dict
-
-def get_entry_list(entry_box_list):
-    entry_list = []
-    for i in range(len(entry_box_list)):
-        entry_list.append(entry_box_list[i].get())
-    return entry_list
-
-def get_dict_from_entries(entry_list):
-    book_dict = {}
-    for i in range(0, len(entry_list), 2):
-        try:
-            book_dict.update({int(entry_list[i+1]) : str(entry_list[i])})
-        except ValueError:
-            break
-    return book_dict
